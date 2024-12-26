fe95b2d6a82b6e37535946e973bbeddba0177fcf
luoyi <1023583830@qq.com>
2024-12-26 10:40:57 +0800

    feat:Add drupal version lua

--- pkg/stage/plugins/drupal.lua
+++ pkg/stage/plugins/drupal.lua
@@ -1,18 +1,43 @@
 function Analyze(info)
     info.Extra = info.Extra or {}
     
-    -- 尝试从 banner获取版本
-    local banner_info = info.Banner
-    if banner_info then
-        local BannerVersion = string.match(banner_info, 'min%.css%?ver=([0-9.]+)')
+    -- 尝试从 response Banner 获取版本
+    if info.Banner then
+        -- 从 meta 标签获取版本号
+        local BannerVersion = string.match(info.Banner, 'content="Drupal%s*([%d%.x%-]+)"')
+        
+        -- 从 site-version class 获取版本
         if not BannerVersion then
-            BannerVersion = string.match(info.Banner, 'min%.js%?ver=([0-9.]+)')
+            BannerVersion = string.match(info.Banner, 'class="site%-version">([%d%.x%-]+)')
+        end
+        
+        -- 从 CHANGELOG.txt 获取版本
+        if not BannerVersion and string.match(info.Banner, 'Drupal%s+1%.0%.0') then
+            BannerVersion = "1.0.0"
         end
         
         if BannerVersion then
-            info.Extra.wordpressversion = BannerVersion
+            info.Extra.drupalversion = BannerVersion
             return info
         end
     end
+
+    -- 尝试从 X-Generator header 获取版本
+    local generator = info.Headers["X-Generator"] or info.Headers["x-generator"]
+    if generator then
+        info.Extra.drupalversion = string.match(generator, "Drupal%s*([%d%.x%-]+)")
+        if info.Extra.drupalversion then
+            return info
+        end
+    end
+
+    -- 尝试从 Server header 获取版本
+    local serverHeader = info.Headers["Server"]
+    if serverHeader then
+        info.Extra.drupalversion = string.match(serverHeader, "Drupal.*?([%d%.x%-]+)")
+        if info.Extra.drupalversion then
+            return info
+        end
+    end
+    return info
 end
-  
