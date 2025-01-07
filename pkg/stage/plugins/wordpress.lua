dd1bbf209d30bcfdbcea8dec9bf98d6baaa7bed0
luoyi <1023583830@qq.com>
2025-01-07 15:50:56 +0800

    fix:modify_wordpress_version

--- pkg/stage/plugins/wordpress.lua
+++ pkg/stage/plugins/wordpress.lua
@@ -4,9 +4,9 @@ function Analyze(info)
     -- 尝试从 banner获取版本
     local banner_info = info.Banner
     if banner_info then
-        local BannerVersion = string.match(banner_info, 'min%.css%?ver=([0-9.]+)')
+        local BannerVersion = string.match(banner_info, 'wp%-emoji%-release%.min%.js%?ver=([0-9.]+)')
         if not BannerVersion then
-            BannerVersion = string.match(info.Banner, 'min%.js%?ver=([0-9.]+)')
+            BannerVersion = string.match(banner_info, 'install%.min%.css%?ver=([0-9.]+)')
         end
         
         if BannerVersion then
@@ -15,4 +15,3 @@ function Analyze(info)
         end
     end
 end
-  
